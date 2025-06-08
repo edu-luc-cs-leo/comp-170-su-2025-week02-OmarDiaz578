5fdd5fe0de93cf24637eb601975c16eb31063f93
OmarDiaz578 <odiaz5@luc.edu>
2025-06-08 23:43:46 +0000

    2nd commit

--- week02.py
+++ week02.py
@@ -3,3 +3,14 @@
 def greet(name):
     return f"Hello {name}. How are you?"
 print(greet("Omar"))
+
+#Greet a few friends
+
+friends_list = ["Sam", "Porter", "Ramzi"]
+
+def greet_friends(friends_list):
+    for friend in friends_list:
+        message = greet(friend)
+        print(message)
+
+greet_friends(friends_list)
